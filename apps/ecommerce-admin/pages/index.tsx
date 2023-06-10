58667aa5a7e1185b742dd7f86e7ed8b6184576fc
rbrtj <robertjaszczurek@icloud.com>
2023-06-10 19:41:28 +0200

    Tailwind installation for ecommerce-admin

--- apps/ecommerce-admin/pages/index.tsx
+++ apps/ecommerce-admin/pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-300">Pages dir works</h1>
+      <h1 className="text-4xl text-red-300">Pages dir works</h1>
       <Button />
     </>
   );

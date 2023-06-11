94ca3521650af012d402b5200aeed6b984662cda
rbrtj <robertjaszczurek@icloud.com>
2023-06-11 17:47:08 +0200

    Mongodb connection

--- apps/ecommerce-admin/pages/api/auth/[...nextauth].js
+++ apps/ecommerce-admin/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from '../../../../../packages/lib/mongodb';
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,5 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });

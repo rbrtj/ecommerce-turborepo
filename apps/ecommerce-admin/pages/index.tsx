935d3939b962d1ae678231be2147c1069d6ac9db
rbrtj <robertjaszczurek@icloud.com>
2023-06-11 17:29:53 +0200

    Moved admin app to a 3000 port

--- apps/ecommerce-admin/pages/index.tsx
+++ apps/ecommerce-admin/pages/index.tsx
@@ -1,13 +1,26 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import { Button } from 'ui';
+import { useSession, signIn, signOut } from 'next-auth/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-4xl text-red-300">Pages dir works</h1>
-      <Button />
-    </>
-  );
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <>
+        <div className="bg-blue-900 w-screen h-screen flex items-center">
+          <div className="text-center w-full">
+            <button
+              onClick={() => signIn('google')}
+              className="bg-white p-2 px-4 rounded-lg"
+            >
+              Login with Google
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return <p>Signed as {session.user.email}</p>;
 }

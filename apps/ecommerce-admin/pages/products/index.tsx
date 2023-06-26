74ce1ff9ceb4de1cd261d1237b40b7a92ca487c6
rbrtj <robertjaszczurek@icloud.com>
2023-06-26 20:42:14 +0200

    Basic products table ui

--- apps/ecommerce-admin/pages/products/index.tsx
+++ apps/ecommerce-admin/pages/products/index.tsx
@@ -3,6 +3,7 @@ import { Button } from 'ui';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Layout } from '../../components/Layout';
+import { Edit } from 'lucide-react';
 
 export interface Product {
   _id: string;
@@ -23,22 +24,34 @@ const Products = () => {
       <Button>
         <Link href={'/products/new'}>Add new product</Link>
       </Button>
-      <table>
-        <thead>
-          <tr>
-            <td>Product name</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product._id}>
-              <td>{product.title}</td>
-              <td>Buttons</td>
+      <div className="relative w-[80%] mx-auto mt-2 overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-3">Product name</th>
+              <th className="px-6 py-3 flex justify-center">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr
+                key={product._id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <td className="px-6 py-4">{product.title}</td>
+                <td className="px-6 py-4 flex justify-center">
+                  <Link href={'/products/edit/' + product._id}>
+                    <Button className="inline-flex gap-2">
+                      <Edit size="16px" />
+                      Edit
+                    </Button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
 };
